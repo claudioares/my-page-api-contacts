e0cfb862043ef1e98268e3b1b849fa1b8ab0db64
claudioares <claudiojasoares.dev@gmail.com>
2024-04-22 22:24:17 -0300

    fix: removendo codigos irrelevantes

--- src/routes/create.messege.client.ts
+++ src/routes/create.messege.client.ts
@@ -1,38 +1,22 @@
-import  { FastifyInstance } from "fastify"
+import { FastifyInstance } from "fastify"
 import { IMessegeClient } from "../interfaces/interfaces";
 import { UsecaseMessegeClient } from "../usecase/usecase.messege.client";
-import { sendNodeMailer } from "../services/nodeMailer";
 
-interface IResultCaseType {
-    id: string,
-    name: string,
-    email: string,
-    messege: string,
-    createdAt: Date
-}
+export async function CreteEvents(app: FastifyInstance) {
 
-export async function CreteEvents (app: FastifyInstance) {
+    app.post("/messegeclient", async (request, reply) => {
 
-    app.post("/messegeclient", async (request, reply)=>{
-
-        const data:IMessegeClient = request.body as IMessegeClient;
+        const data: IMessegeClient = request.body as IMessegeClient;
 
         try {
             const usecase = new UsecaseMessegeClient();
             const resultUseCase = await usecase.create(data);
 
-            const {
-                email, name
-            } = resultUseCase as IResultCaseType
+            return reply.status(201).send({ data: { resultUseCase }});
 
-
-            const sendResult = sendNodeMailer(email, name);
-
-            return reply.status(201).send({data:{resultUseCase}, email:sendResult});
-            
         } catch (error) {
             console.error('Error during event create:', error);
-            reply.status(500).send({ error: "Error during creation!" });
+            return reply.status(500).send({ error: "Error during creation!" });
         }
     })
 }
